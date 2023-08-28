d5a8d5c0f62e674bb6ff3fd418156943a68d05d3
Magna011 <129706360+Magna011@users.noreply.github.com>
2023-08-28 19:20:17 +0700

    Initial commit

---
+++ src/Pelatihan/HomePageFinalQuis/HomePageFinalQuis.jsx
@@ -0,0 +1,32 @@
+import FinalQuis from "../../komponents/HomePage/FinalQuis";
+import FinalKuis from "../../komponents/Modul/FinalQuis";
+import ProgressCardFinalQuis from "../../komponents/ProgressCard/ProgressCardFinalQuis";
+import UlasanFinalQuis from "../../komponents/Ulasan/UlasanFinalQuis";
+
+const HomePageFinalQuis = () => {
+  return (
+    <div>
+      <FinalQuis/>
+      <div className="hero min-h-screen bg-base-200 p-3 m-4">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <div>
+              <ProgressCardFinalQuis />
+              <UlasanFinalQuis/>
+            </div>
+            <div></div>
+          </div>
+          <div className="card flex justify-left items-left w-[400px] h-[700px] m-2 ">
+            <div className="card-body">
+              <div>
+                <FinalKuis />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePageFinalQuis;
